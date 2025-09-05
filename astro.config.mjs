97598c373f7dce3f39f8fb6a34f50a6bebea031b
Asib Kamalsada <asib1999@gmail.com>
2025-09-05 12:34:39 +0200

    fix: removed prefix unkraut from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://asibkamalsada.github.io',
+    site: 'https://www.hamza-bistro-leipzig.de',
     vite: {
         plugins: [tailwindcss()]
     },
-    base: '/unkraut'
 });
