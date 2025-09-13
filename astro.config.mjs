f371c7a5806204cccb33891671a2d8991127ad87
Asib Kamalsada <asib1999@gmail.com>
2025-09-13 18:36:18 +0200

    feat: Switch to ms-konzept.de

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.hamza-bistro-leipzig.de',
+    site: 'https://www.ms-konzept.de',
     vite: {
         plugins: [tailwindcss()]
     },
